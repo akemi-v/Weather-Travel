99e0279e5496330610506d52b07d6072979cc1c7
Maria <mari.semakova.a@gmail.com>
2018-01-07 19:58:43 +0300

    Фикс названий протоколов, добавлено документирование

---
+++ WeatherTravel/Networking/Parsers/SMAParserProtocol.h
@@ -0,0 +1,25 @@
+//
+//  SMAParserProtocol.h
+//  WeatherTravel
+//
+//  Created by Maria Semakova on 1/7/18.
+//  Copyright © 2018 Maria Semakova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+/**
+ Протокол JSON парсера
+ */
+@protocol SMAParserProtocol <NSObject>
+
+/**
+ Парсинг
+
+ @param data Данные для парсинга
+ @return Модель данных, полученная в результате парсинга
+ */
+- (id)parse:(NSData *)data;
+
+@end
---
+++ WeatherTravel/Networking/Requests/SMARequestProtocol.h
@@ -0,0 +1,25 @@
+//
+//  SMARequestProtocol.h
+//  WeatherTravel
+//
+//  Created by Maria Semakova on 1/7/18.
+//  Copyright © 2018 Maria Semakova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+/**
+ Протокол реквестов
+ */
+@protocol SMARequestProtocol <NSObject>
+
+/**
+ Получение реквеста
+
+ @param parameters Параметры, необходимые для включения в реквест
+ @return Реквест
+ */
+- (NSMutableURLRequest *)getUrlRequestWithParameters:(NSDictionary *)parameters;
+
+@end
