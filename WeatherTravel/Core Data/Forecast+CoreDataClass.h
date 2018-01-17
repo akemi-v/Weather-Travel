ae3be721175a8d7ebc9a2d445b9cd420f245a0b9
Maria <mari.semakova.a@gmail.com>
2018-01-18 02:17:05 +0300

    Сгенерированы классы кор даты

---
+++ WeatherTravel/Core Data/Forecast+CoreDataClass.h
@@ -0,0 +1,21 @@
+//
+//  Forecast+CoreDataClass.h
+//  WeatherTravel
+//
+//  Created by Maria Semakova on 1/18/18.
+//  Copyright © 2018 Maria Semakova. All rights reserved.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Forecast : NSManagedObject
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#import "Forecast+CoreDataProperties.h"
