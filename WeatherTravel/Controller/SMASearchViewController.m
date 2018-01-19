eb86de84286df1676080cdc8e81e31897e207aad
Maria <mari.semakova.a@gmail.com>
2018-01-19 04:55:20 +0300

    Добавлен индикатор на экран поиска

--- WeatherTravel/Controller/SMASearchViewController.m
+++ WeatherTravel/Controller/SMASearchViewController.m
@@ -27,6 +27,7 @@ static const CGFloat SMASearchFieldHeight = 50.f;
  */
 @property (nonatomic, strong) SMAForecastService *forecastService;
 @property (nonatomic, strong) SMAImageLoader *imageLoader;
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
 
 @end
 
@@ -82,6 +83,7 @@ static const CGFloat SMASearchFieldHeight = 50.f;
 {
     self.view.backgroundColor = UIColor.customBlue;
     [self setupSearchField];
+    [self setupActivityIndicator];
 }
 
 - (void)setupSearchField
@@ -89,6 +91,14 @@ static const CGFloat SMASearchFieldHeight = 50.f;
     self.searchField = [[SMALocationSearchField alloc] initWithFrame:CGRectMake(0, CGRectGetMidY(self.view.frame), CGRectGetWidth(self.view.frame), SMASearchFieldHeight)];
     self.searchField.delegate = self;
     [self.view addSubview:self.searchField];
+}
+
+- (void)setupActivityIndicator
+{
+    self.activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.activityIndicator.hidesWhenStopped = YES;
+    self.activityIndicator.center = self.view.center;
+    [self.view addSubview:self.activityIndicator];
 }
 
 - (void)setupConstraints
@@ -124,23 +134,30 @@ static const CGFloat SMASearchFieldHeight = 50.f;
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     [textField resignFirstResponder];
+    [self.activityIndicator startAnimating];
     [UIView animateWithDuration:0.5 animations:^{
         self.forecastView.layer.opacity = 0.1f;
-        self.forecastView.transform = CGAffineTransformMakeScale(0.05f, 0.05f);
+        self.forecastView.transform = CGAffineTransformMakeScale(0.01f, 0.01f);
     }];
-    [self.forecastService getForecastForCityOnline:textField.text completion:^(SMAForecastModel *model) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self.forecastView setupWithForecastModel:model];
-            [self.imageLoader loadImageFromFileURL:model.urlOrigImage completion:^(UIImage *image) {
-                self.forecastView.pictureView.image = image;
-                [self.delegate reload];
-                [UIView animateWithDuration:0.5 animations:^{
-                    self.forecastView.layer.opacity = 1.f;
-                    self.forecastView.transform = CGAffineTransformIdentity;
+    
+    NSString *city = textField.text;
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        [self.forecastService getForecastForCityOnline:city completion:^(SMAForecastModel *model) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.forecastView setupWithForecastModel:model];
+                [self.imageLoader loadImageFromFileURL:model.urlOrigImage completion:^(UIImage *image) {
+                    self.forecastView.pictureView.image = image;
+                    [self.delegate reload];
+                    [self.activityIndicator stopAnimating];
+                    [UIView animateWithDuration:0.5 animations:^{
+                        self.forecastView.layer.opacity = 1.f;
+                        self.forecastView.transform = CGAffineTransformIdentity;
+                    }];
                 }];
-            }];
-        });
-    }];
+            });
+        }];
+    });
+    
     return YES;
 }
 
