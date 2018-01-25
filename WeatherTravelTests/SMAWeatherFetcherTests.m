d2a93b9c4c9c3ba92347fb4bb93eabc5f4bb8e3b
Maria <mari.semakova.a@gmail.com>
2018-01-25 14:07:47 +0300

    Исправлены тесты на SMAWeatherFetcher

--- WeatherTravelTests/SMAWeatherFetcherTests.m
+++ WeatherTravelTests/SMAWeatherFetcherTests.m
@@ -11,6 +11,8 @@
 #import <Expecta/Expecta.h>
 
 #import "SMAWeatherFetcher.h"
+#import "SMADarkSkyCurrentWeatherRequest.h"
+#import "SMADarkSkyCurrentWeatherParser.h"
 
 @interface SMAWeatherFetcherTests : XCTestCase
 
@@ -32,44 +34,67 @@
     [super tearDown];
 }
 
-- (void)testGetWeatherFromCoordinatesNil
+- (void)testGetWeatherFromCoordinatesRequestNil
 {
-    NSDictionary *coordinates = nil;
+    NSDictionary *coordinates = @{@"coo": @"rdinates"};
+    id request = OCMClassMock([SMADarkSkyCurrentWeatherRequest class]);
+    OCMStub(ClassMethod([request getUrlRequestWithParameters:[OCMArg any]])).andReturn(nil);
+    [self.fetcher getWeatherFromCoordinates:coordinates completion:nil];
+    OCMVerify(ClassMethod([request getUrlRequestWithParameters:coordinates]));
     
-    __block BOOL isCalled = NO;
-    __block NSDictionary *weatherInfo = nil;
-    [self.fetcher getWeatherFromCoordinates:coordinates completion:^(NSDictionary *weatherData) {
-        isCalled = YES;
-        weatherInfo = weatherData;
-    }];
-    expect(isCalled).after(5).to.beFalsy();
-    expect(weatherInfo).after(5).to.beNil();
+    [request stopMocking];
 }
 
-- (void)testGetWeatherFromCoordinatesReal
+- (void)testGetWeatherFromCoordinates
 {
-    NSDictionary *coordinates = @{
-                                  @"lng": @"139.692",
-                                  @"lat": @"35.6895"
-                                  };
-    NSSet *expectedKeys = [NSSet setWithArray:@[@"temperature", @"humidity", @"summary_weather"]];
+    NSDictionary *coordinates = @{@"coo": @"rdinates"};
     
-    __block BOOL isCalled = NO;
-    __block NSDictionary *weatherInfo = nil;
-    [self.fetcher getWeatherFromCoordinates:coordinates completion:^(NSDictionary *weatherData) {
-        isCalled = YES;
-        weatherInfo = weatherData;
-    }];
-    expect(isCalled).after(5).to.beTruthy();
-    expect(weatherInfo).after(5).toNot.beNil();
-    expect([NSSet setWithArray:[weatherInfo allKeys]]).to.equal(expectedKeys);
+    id request = OCMClassMock([SMADarkSkyCurrentWeatherRequest class]);
+    id urlRequest = OCMClassMock([NSURLRequest class]);
+    OCMStub(ClassMethod([request getUrlRequestWithParameters:[OCMArg any]])).andReturn(urlRequest);
+    
+    id configuration = OCMClassMock([NSURLSessionConfiguration class]);
+    OCMStub(ClassMethod([configuration defaultSessionConfiguration])).andReturn(configuration);
+    
+    id session = OCMClassMock([NSURLSession class]);
+    OCMStub(ClassMethod([session sessionWithConfiguration:configuration])).andReturn(session);
+    
+    id sessionDataTask = OCMClassMock([NSURLSessionDataTask class]);
+    OCMStub([session dataTaskWithRequest:urlRequest completionHandler:[OCMArg any]]).andReturn(sessionDataTask);
+    
+    [self.fetcher getWeatherFromCoordinates:coordinates completion:nil];
+    
+    OCMVerify([sessionDataTask resume]);
+    
+    [request stopMocking];
+    [urlRequest stopMocking];
+    [configuration stopMocking];
+    [session stopMocking];
+    [sessionDataTask stopMocking];
 }
 
-- (void)testGetWeatherFromCoordinatesWrong
+- (void)testGetWeatherFromCoordinatesReceivedDataNil
 {
-    NSDictionary *coordinates = @{
-                                  @"wrong": @"139.692",
-                                  };
+    NSDictionary *coordinates = @{@"coo": @"rdinates"};
+    
+    id request = OCMClassMock([SMADarkSkyCurrentWeatherRequest class]);
+    id urlRequest = OCMClassMock([NSURLRequest class]);
+    OCMStub(ClassMethod([request getUrlRequestWithParameters:[OCMArg any]])).andReturn(urlRequest);
+    
+    id configuration = OCMClassMock([NSURLSessionConfiguration class]);
+    OCMStub(ClassMethod([configuration defaultSessionConfiguration])).andReturn(configuration);
+    
+    id session = OCMClassMock([NSURLSession class]);
+    OCMStub(ClassMethod([session sessionWithConfiguration:configuration])).andReturn(session);
+    
+    id error = OCMPartialMock([NSError new]);
+    OCMStub([error localizedDescription]).andReturn(nil);
+    
+    id sessionDataTask = OCMClassMock([NSURLSessionDataTask class]);
+    OCMStub([session dataTaskWithRequest:urlRequest completionHandler:([OCMArg invokeBlockWithArgs:[NSNull null], [OCMArg any], error, nil])]).andReturn(sessionDataTask);
+    
+    id parser = OCMClassMock([SMADarkSkyCurrentWeatherParser class]);
+    OCMStub(ClassMethod([parser parse:[OCMArg any]]));
     
     __block BOOL isCalled = NO;
     __block NSDictionary *weatherInfo = nil;
@@ -77,8 +102,100 @@
         isCalled = YES;
         weatherInfo = weatherData;
     }];
-    expect(isCalled).after(5).to.beFalsy();
-    expect(weatherInfo).after(5).to.beNil();
+    
+    OCMVerify([sessionDataTask resume]);
+    OCMVerify([error localizedDescription]);
+    OCMReject([parser parse:[OCMArg any]]);
+    expect(weatherInfo).to.beNil();
+    expect(isCalled).to.beFalsy();
+    
+    [request stopMocking];
+    [urlRequest stopMocking];
+    [configuration stopMocking];
+    [session stopMocking];
+    [error stopMocking];
+    [sessionDataTask stopMocking];
+    [parser stopMocking];
+}
+
+- (void)testGetWeatherFromCoordinatesReceivedDataCorrect
+{
+    NSDictionary *coordinates = @{@"coo": @"rdinates"};
+    
+    id request = OCMClassMock([SMADarkSkyCurrentWeatherRequest class]);
+    id urlRequest = OCMClassMock([NSURLRequest class]);
+    OCMStub(ClassMethod([request getUrlRequestWithParameters:[OCMArg any]])).andReturn(urlRequest);
+    
+    id configuration = OCMClassMock([NSURLSessionConfiguration class]);
+    OCMStub(ClassMethod([configuration defaultSessionConfiguration])).andReturn(configuration);
+    
+    id session = OCMClassMock([NSURLSession class]);
+    OCMStub(ClassMethod([session sessionWithConfiguration:configuration])).andReturn(session);
+    
+    id sessionDataTask = OCMClassMock([NSURLSessionDataTask class]);
+    OCMStub([session dataTaskWithRequest:urlRequest completionHandler:([OCMArg invokeBlockWithArgs:[OCMArg isNotNil], [OCMArg any], [NSNull null], nil])]).andReturn(sessionDataTask);
+    
+    NSDictionary *expectedResult = @{@"expected": @"result"};
+    id parser = OCMClassMock([SMADarkSkyCurrentWeatherParser class]);
+    OCMStub(ClassMethod([parser parse:[OCMArg any]])).andReturn(expectedResult);
+    
+    __block BOOL isCalled = NO;
+    __block NSDictionary *result = nil;
+    [self.fetcher getWeatherFromCoordinates:coordinates completion:^(NSDictionary *weatherData) {
+        isCalled = YES;
+        result = weatherData;
+    }];
+    
+    OCMVerify([sessionDataTask resume]);
+    expect(result).to.equal(expectedResult);
+    expect(isCalled).to.beTruthy();
+    
+    [request stopMocking];
+    [urlRequest stopMocking];
+    [configuration stopMocking];
+    [session stopMocking];
+    [sessionDataTask stopMocking];
+    [parser stopMocking];
+}
+
+
+- (void)testGetWeatherFromCoordinatesReceivedDataIncorrect
+{
+    NSDictionary *coordinates = @{@"coo": @"rdinates"};
+    
+    id request = OCMClassMock([SMADarkSkyCurrentWeatherRequest class]);
+    id urlRequest = OCMClassMock([NSURLRequest class]);
+    OCMStub(ClassMethod([request getUrlRequestWithParameters:[OCMArg any]])).andReturn(urlRequest);
+    
+    id configuration = OCMClassMock([NSURLSessionConfiguration class]);
+    OCMStub(ClassMethod([configuration defaultSessionConfiguration])).andReturn(configuration);
+    
+    id session = OCMClassMock([NSURLSession class]);
+    OCMStub(ClassMethod([session sessionWithConfiguration:configuration])).andReturn(session);
+    
+    id sessionDataTask = OCMClassMock([NSURLSessionDataTask class]);
+    OCMStub([session dataTaskWithRequest:urlRequest completionHandler:([OCMArg invokeBlockWithArgs:[OCMArg isNotNil], [OCMArg any], [NSNull null], nil])]).andReturn(sessionDataTask);
+    
+    id parser = OCMClassMock([SMADarkSkyCurrentWeatherParser class]);
+    OCMStub(ClassMethod([parser parse:[OCMArg any]])).andReturn(nil);
+    
+    __block BOOL isCalled = NO;
+    __block NSDictionary *result = nil;
+    [self.fetcher getWeatherFromCoordinates:coordinates completion:^(NSDictionary *weatherData) {
+        isCalled = YES;
+        result = weatherData;
+    }];
+    
+    OCMVerify([sessionDataTask resume]);
+    expect(result).to.beNil();
+    expect(isCalled).to.beTruthy();
+    
+    [request stopMocking];
+    [urlRequest stopMocking];
+    [configuration stopMocking];
+    [session stopMocking];
+    [sessionDataTask stopMocking];
+    [parser stopMocking];
 }
 
 @end
